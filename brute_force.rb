cb76910d6180cd4b6ab259da58a9145b9c76b30e
Stephen <sblackstone@gmail.com>
2019-03-06 21:59:27 -0500

    latest

--- brute_force.rb
+++ brute_force.rb
@@ -1,40 +1,28 @@
+base_limit = 1000
+exp_limit = 20
 
-# a^{m}+b^{n}=c^{k}\quad
 
-# a,b,c co-prime
-def bases(limit)
-  2.upto(limit) do |a|
-    2.upto(limit) do |b|
-      2.upto(limit) do |c|
-        yield a,b,c if a.gcd(b) == 1 and b.gcd(c) == 1 #and a == 2 and b == 7 and c == 3
-      end      
-    end  
-  end
-end
-
-def exponents(limit)  
-  2.upto(limit) do |m|
-    2.upto(limit) do |n|
-      2.upto(limit) do |k|
-        next if m == n and n ==k #flt
-        yield m,n,k #if m == 5 and n == 2 and k == 4
-      end      
-    end    
+2.upto(base_limit) do |a|
+  2.upto(base_limit) do |b|
+    next unless a.gcd(b) == 1
+    2.upto(base_limit) do |c|
+      next unless b.gcd(c) == 1
+      2.upto(exp_limit) do |m|
+        a_m = a**m
+        2.upto(exp_limit) do |n|
+          b_n = b**n
+          2.upto(exp_limit) do |k|
+            next if m == n and n ==k #flt
+            c_k = c**k
+            if a_m + b_n == c_k
+              if (1.0 / m) + (1.0 / n) + (1.0 / k) < 1.0
+                puts "%d^{%d} + %d^{%d} = %d^{%d}" % [a,m,b,n,c,k]
+              end
+              break if c_k > a_m + b_n                              
+            end    
+          end      
+        end    
+      end
+    end      
   end  
 end
-
-bases(20) do |a,b,c|
-  exponents(20) do |m,n,k|
-    #puts "%d^{%d} + %d^{%d} = %d^{%d}" % [a,m,b,n,c,k]
-    #puts "LHS: %d" % [ (a**m) + (b**n) ]
-    #puts "RHS: %d" % [ c**k ]
-    if (a**m) + (b**n) == c**k #or true
-      if (1.0 / m) + (1.0 / n) + (1.0 / k) < 1.0
-        puts "%d^{%d} + %d^{%d} = %d^{%d}" % [a,m,b,n,c,k]
-      end
-    end    
-  end
-end
-
-
-
